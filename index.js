816c61232554e2173ac8e3102247d4b81daefe85
Yash <yashvbhedaru@gmail.com>
2021-02-26 01:37:52 -0800

    see if simple json works

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var quote = document.getElementById('quote')
 
-const res =  fetch("https://quotes.rest/qod?category=inspire").then(res=>res.json()).then(data =>quote.innerHTML=data.contents.quotes[0].quote );
+const res =  ("Have a nice day","Hello","Believe in yourself").then(res=>res.json()).then(data =>quote.innerHTML=data.contents.quotes[0].quote );
